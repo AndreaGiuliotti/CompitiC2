1dee0071d5df2f2a2cd10a22a4e49ced52175304
Andrea Giuliotti <andrea.giuliotti@outlook.it>
2022-04-08 11:24:06 -0700

    Sesto commit: utilizzo della funzione strcat()

--- repoClonata/fileStringhe.c
+++ repoClonata/fileStringhe.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 int main(){
 
-	char c[] = {'a', 'n', 'd', 'r', 'e', 'a'};    
-   	char c2[20];
-   	strcpy(c2,c);    
-   	printf("La seconda stringa contiente:  %s \n",c2); 
+	 
 	
 	return 0;
 }
