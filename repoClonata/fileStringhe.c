dca1a270ca8eb78bedad819f718a7a2e7316391a
Andrea Giuliotti <andrea.giuliotti@outlook.it>
2022-04-08 11:44:15 -0700

    Ottavo commit: utilizzo della funzione strrev()

--- repoClonata/fileStringhe.c
+++ repoClonata/fileStringhe.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 int main(){
 
-	
+	char str[20];
+  printf("Inserisci una stringa: ");    
+  gets(str); 
+  printf("La stringa inserita è: %s",str);    
+  printf("\nLa stringa scritta al contrario è: %s",strrev(str));   	
 	
 	return 0;
 }
