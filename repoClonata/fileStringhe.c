079609eea12b62dccd3d13a82c14a82b570db85c
Andrea Giuliotti <andrea.giuliotti@outlook.it>
2022-04-08 12:13:45 -0700

    Decimo commit: utilizzo della funzione strupr

--- repoClonata/fileStringhe.c
+++ repoClonata/fileStringhe.c
@@ -1,7 +1,11 @@
+#include <string.h>
 #include <stdio.h>
 int main(){
 
-		
-	
+	char str[20];    
+  printf("Inserisci stringa: ");    
+  gets(str);
+  printf("La stringa inserita è: %s",str);    
+  printf("\nLa stringa in maiuscolo è: %s",strupr(str)); 
 	return 0;
 }
